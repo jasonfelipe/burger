ff114106a1f50e063f3243c2cf25843a6e727934
Legend <jasonrfelipe@gmail.com>
2018-08-07 18:38:57 -0700

    added insert code into orm, added orm into burger.js

--- config/orm.js
+++ config/orm.js
@@ -1,24 +1,42 @@
 const connection = require('../config/connection.js');
 
 
-//Function that makes object/key pairs into SQL syntax
-function objToSql(ob) {
+// Helper function for SQL syntax.
+// Let's say we want to pass 3 values into the mySQL query.
+// In order to write the query, we need 3 question marks.
+// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
+// ["?", "?", "?"].toString() => "?,?,?";
+function printQuestionMarks(num) {
     let arr = [];
 
-    //Loop through keys and push the key/value as a string int arr
-    for (var key in ob) {
-        let value = ob[key];
-        //check to skip hidden properties
-        if (Object.hasOwnProperty.call(ob, key)) {
-            //checks if there is a space, if there is
-            //then it adds quotes to it
-            if (typeof value === "string" && value.indexOf(" ") >= 0)
-                value = " " + value + "'";
-        }
-        //Example: {burger: 'cheeseburger'} to
-        //["burger='burger'"]  
-        arr.push(key + "=" + value);
+    for (let i = 0; i < num; i++) {
+        arr.push("?");
     }
+
+    return arr.toString();
+}
+
+//Function that makes object/key pairs into SQL syntax
+function objToSql(ob) {
+    var arr = [];
+
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+        var value = ob[key];
+        // check to skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)) {
+            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+            // e.g. {sleepy: true} => ["sleepy=true"]
+            arr.push(key + "=" + value);
+        }
+    }
+
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
 }
 
 const orm = {
@@ -34,8 +52,27 @@ const orm = {
 
     },
 
-    insertOne: function () {
+    insertOne: function (table, cols, vals, cb) {
         console.log("This inserts one");
+
+        let queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function(err, result){
+            if (err){
+                console.log(err);
+            }
+
+            cb(reuslt);
+        });
     },
 
     updateOne: function () {
