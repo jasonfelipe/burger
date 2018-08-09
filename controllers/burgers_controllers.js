c36c76fb4896949df133243925bad99fa1826236
Legend <jasonrfelipe@gmail.com>
2018-08-09 00:12:09 -0700

    added more code and tested the website

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -19,7 +19,7 @@ router.post("/api/burgers", function (req, res) {
     burger.create([
         "burger_name", "devoured"
     ], [
-            req.body.name, req.body.sleepy
+            req.body.burger_name, req.body.devoured
         ], function (result) {
             // Send back the ID of the new burger
             res.json({ id: result.insertId });
