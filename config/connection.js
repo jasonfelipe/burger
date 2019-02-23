fee80736fdc9273e03295ecf0968cf53a96fc60b
Legend <jasonrfelipe@gmail.com>
2019-02-23 10:20:02 -0700

    adding database stuff

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "l7cup2om0gngra77.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
   user: "root",
-  password: "root",
-  database: "burgers_db"
+  password: "ygkdud08k4c6ob67",
+  database: "oztdyogeb75xeu4u"
 });
 
 // Make connection.
